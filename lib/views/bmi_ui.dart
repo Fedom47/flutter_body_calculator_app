e4052c88b85da2b16a41e2b652e81e607bc83e96
Fedom47 <fedom47@gmail.com>
2026-04-01 13:09:16 +0700

    edit about ui page and bmi

--- lib/views/bmi_ui.dart
+++ lib/views/bmi_ui.dart
@@ -10,6 +10,23 @@ class BmiUi extends StatefulWidget {
 class _BmiUiState extends State<BmiUi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 40, left: 30, right: 30, bottom: 50,
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                //ส่วนซื้อหน้าจอ และรูป
+                //ส่วนการป้อนข้อมูล
+                //ส่วนปุ่ม
+                //ส่วนแสดงผลข้อมูลที่ไดจากการคำนวน
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_calculator_app/views/about_ui.dart';
+import 'package:flutter_body_calculator_app/views/bmi_ui.dart';
+import 'package:flutter_body_calculator_app/views/bmr_ui.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUi extends StatefulWidget {
@@ -9,7 +12,12 @@ class HomeUi extends StatefulWidget {
 }
 
 class _HomeUiState extends State<HomeUi> {
-  @override
+  //สร้างตัวแปรเพื่อเก็บค่าหมายเลขของหน้าที่ถูกเลือกใน BottomNavigationBar
+  int selectedIndex = 1;
+  //สร้างตัวแปรเพื่อเก็บหน้าที่จะแสดงใน Body ตามหมายเลขที่ถูกเลือกใน BottomNavigationBar
+  //ข้อมูลในตัวแปรนี้จะเป็น List จะมีindex 0,1,2 ตามลำดับของหน้าที่ถูกสร้างขึ้นมา
+  List subPage = [BmiUi(), AboutUi(), BmrUi()];
+
   Widget build(BuildContext context) {
     return Scaffold(
       //ส่วนของ AppBar-----------------------------------------
@@ -23,14 +31,17 @@ class _HomeUiState extends State<HomeUi> {
       ),
       //ส่วนของ ButtomNavigationBar-----------------------------------------
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        currentIndex: selectedIndex,
         selectedItemColor: Colors.deepOrange,
         unselectedItemColor: Colors.grey,
+        showUnselectedLabels: false,
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'BMI',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'BMI'),
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'เกี่ยวกับเรา',
@@ -41,6 +52,8 @@ class _HomeUiState extends State<HomeUi> {
           ),
         ],
       ),
+      //ส่วนของ Body-----------------------------------------
+      body: subPage[selectedIndex],
     );
   }
 }
